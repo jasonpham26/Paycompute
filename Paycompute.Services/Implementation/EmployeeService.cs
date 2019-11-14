a20f9ec37b3bc7e7ef4717e1317cece84b28cdc9
Jason Pham <vinhphuong0206@gmail.com>
2019-11-15 00:10:42 +1000

    Added EmployeeIndex view model

---
+++ Paycompute.Services/Implementation/EmployeeService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Paycompute.Entity;
+using Paycompute.Persistence;
+
+namespace Paycompute.Services.Implementation
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeeService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateAsync(Employee newEmployee)
+        {
+            await _context.Employees.AddAsync(newEmployee);
+            await _context.SaveChangesAsync();
+        }
+
+        public Employee GetById(int employeeID) => 
+            _context.Employees.Where(e => e.Id == employeeID).FirstOrDefault();
+
+        public async Task Delete(int employeeID)
+        {
+            var employee = GetById(employeeID);
+            _context.Remove(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<Employee> GetAll() => _context.Employees;
+
+        public async Task UpdateAsync(Employee employee)
+        {
+            _context.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(int id)
+        {
+            var employee = GetById(id);
+            _context.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public decimal StudentLoanRepaymentAmount(int id, decimal totalAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal UnionFees(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
